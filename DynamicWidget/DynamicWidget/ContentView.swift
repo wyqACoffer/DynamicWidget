5982498c1407bac1859020df62763638a25566cb
行勇 <wangyanqi.wyq@alibaba-inc.com>
2023-10-23 15:58:18 +0800

    [1023][feat]创建工程

---
+++ DynamicWidget/DynamicWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicWidget
+//
+//  Created by 行勇 on 2023/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("开启实时活动")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicWidget/DynamicWidget/DynamicWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicWidgetApp.swift
+//  DynamicWidget
+//
+//  Created by 行勇 on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DynamicWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
