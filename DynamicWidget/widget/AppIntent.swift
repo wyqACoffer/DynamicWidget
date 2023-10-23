bf834e685b57796bbb7194347f7bc1320feabed9
行勇 <wangyanqi.wyq@alibaba-inc.com>
2023-10-23 17:28:28 +0800

    [1023][feat]动态小组件

---
+++ DynamicWidget/widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  widget
+//
+//  Created by 行勇 on 2023/10/23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ DynamicWidget/widget/widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by 行勇 on 2023/10/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetLiveActivity()
+    }
+}
